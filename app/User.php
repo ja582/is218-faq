25bb36662642113eddd9d97e3c71bd50a6914e7f
joe <ja582@njit.edu>
2019-08-01 16:57:20 -0400

    Established Question part of project: - Question Migration - Question Factory - Question Seeder - Question PHPUnit Test - User profiles can now create Questions

--- app/User.php
+++ app/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 }
